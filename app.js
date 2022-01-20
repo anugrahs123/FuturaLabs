21b8ab319fd2319dbfdb06cc12703fc0a5654545
anugrahs123 <anugrahsasikumar@gmail.com>
2022-01-20 16:09:10 +0530

    aq

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const res = require('express/lib/response');
+const bookdata = require('./src/model/data1');
 const app = new express();
+// const bookdata = require('../model/data1');
 
 app.use(express.static('public'));
 app.use('/css',express.static(__dirname+'/public/css'));
@@ -21,6 +23,26 @@ app.get("/",function(req,res){
 app.get("/sign",function(req,res){
 
     res.render("register");
+
+});
+
+app.get("/sign/add",function(req,res){
+    
+    var item = {
+        Email: req.query.Email,
+        Password: req.query.Password,
+        Phone: req.query.Phone,
+        Address: req.query.Address,
+        Address2: req.query.Address2,
+        City: req.query.City,
+        State: req.query.State,
+        Zip: req.query.Zip
+
+    }
+    var book = bookdata(item);
+    book.save();
+    res.redirect('/');
+
 
 });
 
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const adminRouter = express.Router();
+const bookdata = require('../model/data1');
 
 function router(){
     adminRouter.get('/',function(req,res){
         res.send("heyy");
+
+        // var item = {
+        //     req.query.Email;
+        //     req.query.Password;
+        //     req.query.Phone;
+        //     req.query.Address;
+        //     req.query.Address2;
+        //     req.query.City;
+        //     req.query.State;
+        //     req.query.Zip;
+    
+        // }
+        // var book = bookdata(item);
+        // book.save();
     })
 
 
